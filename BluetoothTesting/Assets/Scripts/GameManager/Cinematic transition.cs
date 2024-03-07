81619ef9839841c4599fcd8676087e96cab10f92
Bulancea Matei <67746218+Oberonjr@users.noreply.github.com>
2024-03-07 16:28:19 +0100

    Improvements to scripts, attempt to fix level

--- BluetoothTesting/Assets/Scripts/GameManager/Cinematic transition.cs
+++ BluetoothTesting/Assets/Scripts/GameManager/Cinematic transition.cs
@@ -2,11 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class Cinematictransition : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    [SerializeField] private float cinMoveSpeed;
+    [SerializeField] private float cinTurnSpeed;
+    private bool hasEntered;
     private GameObject player;
+    private Camera camera;
+   
+
+    
 
     private void OnTriggerEnter(Collider other)
     {
@@ -14,15 +22,38 @@ public class Cinematictransition : MonoBehaviour
         {
             player = other.gameObject;
             player.GetComponent<PlayerMovement>().canMove = false;
-            GoToCinematic(player);
+            Vector3 newPosition = transform.position;
+            newPosition.y = player.transform.position.y;
+            transform.position = newPosition;
+            hasEntered = true;
+            camera = player.GetComponentInChildren<Camera>();
+            
         }
     }
 
+    void FixedUpdate()
+    {
+        if (hasEntered)
+        {
+            GoToCinematic(player);
+        }
+    }
+    
     void GoToCinematic(GameObject _player)
     {
         Vector3 direction = target.position - _player.transform.position;
+        float distance = Vector3.Distance(_player.transform.position, target.position);
         Quaternion targetRotation = Quaternion.LookRotation(direction);
-        _player.transform.rotation = Quaternion.Lerp(_player.transform.rotation, targetRotation, 0.3f);
-        
+        _player.transform.rotation = Quaternion.Lerp(_player.transform.rotation, targetRotation, 0.035f);
+        if (distance > 2f)
+        {
+             _player.GetComponent<PlayerMovement>().characterController.Move(direction * 0.04f * Time.deltaTime);
+            camera.fieldOfView += 0.05f;
+            
+            
+        }
+
     }
+
+    
 }
